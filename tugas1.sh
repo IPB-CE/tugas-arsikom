4737d049fbf769c25ed4660d65ff66692587232c
NRF <contact.admin@naufalrf.me>
2023-08-16 15:14:58 +0700

    Create tugas1.sh

---
+++ tugas1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Tugas: Latihan Perintah Dasar Linux
+# Deadline: [Tanggal Deadline]
+
+# Instruksi:
+# 1. Hapus komentar pada setiap soal dan masukan perintah sesuai dengan instruksi yang diberikan.
+# 2. Ubah dengan teks editor yang ada pada vim contoh: nano/vim
+# 3. Jalankan script ini dan perhatikan output yang dihasilkan.
+# 4. Pahami fungsi dari setiap perintah.
+# 5. Screenshot code dan jalankan outputnta
+
+# Contoh Tugas:
+echo "Contoh tugas 1: Informasi Sistem Operasi"
+uname -a 
+echo ""
+
+# Menampilkan daftar direktori saat ini
+echo "Contoh Tugas 2: Daftar Direktori Saat Ini"
+ls
+echo ""
+
+
+# Membuat direktori baru
+echo "Tugas 1: Membuat Direktori Baru"
+# TODO: Tambahkan perintah linux untuk membuat direktori baru dengan nama 'my_dir' di bawah ini
+echo ""
+
+# Pindah ke direktori baru
+echo "Tugas 2: Pindah ke Direktori Baru"
+# TODO: Tambahkan perintah linux untuk pindah ke direktori 'my_dir' di bawah ini
+echo ""
+
+# Membuat file teks
+echo "Tugas 3: Membuat File Teks"
+# TODO: Tambahkan perintah linux untuk membuat file teks dengan nama 'my_file.txt' dan isi bebas di bawah ini
+echo ""
